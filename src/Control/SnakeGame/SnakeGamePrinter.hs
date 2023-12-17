5acb128572417ab4c28dd422418e99a0225c7761
Antonio Ponce <AntonioJuanPonce@outlook.com>
2023-12-17 11:02:01 -0600

    remove redudant check

--- src/Control/SnakeGame/SnakeGamePrinter.hs
+++ src/Control/SnakeGame/SnakeGamePrinter.hs
@@ -14,6 +14,7 @@ import Data.Functor (($>))
 import Data.Function (fix)
 import Control.Concurrent (forkIO, threadDelay, killThread)
 import Text.Printf (printf)
+import Data.Foldable (traverse_)
 
 
 input :: Int -> Int -> NominalDiffTime -> IO (GameInput IO)
@@ -25,34 +26,30 @@ input width height delay = do
         then do 
             writeIORef first' False
             draw game
-            fix \loop -> getKey >>= \key -> if isDir key
-                then writeIORef key' (toDir key) $> toDir key
-                else loop
+            fix \loop -> getKey >>= maybe loop \key ->
+                writeIORef key' key $> key                
         else do
             draw game
             child <- forkIO$ fix \loop -> do 
                 hReady stdin >>= flip when do
-                    key <- getKey
-                    when (isDir key) do
-                        writeIORef key' (toDir key)
+                    getKey >>= traverse_ (writeIORef key')
                 loop        
             threadDelay (fromEnum delay `div` (1000*1000))
             killThread child
             readIORef key'
   where
-    toDir key = case key of
-        "\ESC[A" -> UpD
-        "\ESC[B" -> DownD
-        "\ESC[C" -> RightD
-        "\ESC[D" -> LeftD
-        _        -> error "impossible"
-    isDir key = key `elem` ["\ESC[A","\ESC[B","\ESC[C","\ESC[D"]
-    getKey = reverse <$> getKey' ""
+    getKey = toDir . reverse <$> getKey' ""
       where 
         getKey' chars = do
             char <- getChar
             more <- hReady stdin
             (if more then getKey' else return) (char:chars)
+        toDir key = case key of
+            "\ESC[A" -> Just UpD
+            "\ESC[B" -> Just DownD
+            "\ESC[C" -> Just RightD
+            "\ESC[D" -> Just LeftD
+            _        -> Nothing
     draw game = do 
         clearScreen
         forM_ [0..height - 1] \y -> do
